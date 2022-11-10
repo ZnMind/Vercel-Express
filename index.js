218ca1c18434a4e6d65b9c742e2afc9ba7a2087e
DKProtocol <damann1072@gmail.com>
2022-11-10 13:32:40 -0600

    update

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import express from 'express';
-
+const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
@@ -9,4 +8,4 @@ app.get("/", (req, res) => {
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
 
-export default app;
+module.exports = app;
