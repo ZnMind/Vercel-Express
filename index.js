03f6b734b1c3fbc4e45518569d665b7ee176d9bb
DKProtocol <damann1072@gmail.com>
2022-11-10 14:16:04 -0600

    env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Express on Vercel");
+    res.send(`${process.env.TEST}`);
 })
 
 const port = process.env.PORT || 5000;
